d82db77c9b9eddd37d5253e830734a37b47b310d
Jessica Ulysse <Jessica.Ulysse@bpx.com>
2022-05-17 09:36:29 -0500

    added linear solution

--- Binary_Search/assignment_1.py
+++ Binary_Search/assignment_1.py
@@ -1,4 +1,4 @@
-from jovian.pythondsa import evaluate_test_cases
+from jovian.pythondsa import evaluate_test_case
 
 """
 1. Given a rotated sorted list that was rotated some unknown 
@@ -20,4 +20,25 @@ test = {
     'output': 3
 }
 
-def count_rotations(nums):
+def count_rotations_linear(nums):
+    # what is the initial value of position?
+    position = 0
+
+    # when should the loop be terminated? 
+    while position < len(nums):
+
+        # Success critera: check whether the number at the current position 
+        if position > 0 and nums[position] < nums[position-1]:
+            return position
+
+        # Move to the next position
+        position += 1
+
+    # What if none of the positions passed
+    return 0
+
+    
+evaluate_test_case(count_rotations_linear, test)
+
+
+
